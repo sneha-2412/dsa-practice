8a83e5b3af25ce83c57180e7c10548ecdf8193e1
sneha-2412 <22h41f0052sneha@gmail.com>
2026-02-09 23:54:52 +0530

    Sorting Techniques

---
+++ dsa-practice/src/sorting_techniques/Selection.java
@@ -0,0 +1,28 @@
+package sorting_techniques;
+
+public class Selection {
+	public static void selectionSort(int[] arr,int n)
+	{
+		for(int i=0;i<n;i++)
+		{
+			int minIndex=i;
+			for(int j=i+1;j<n;j++)
+			{
+				if(arr[j]<arr[minIndex])
+					minIndex=j;
+			}
+			int temp=arr[minIndex];
+			arr[minIndex]=arr[i];
+			arr[i]=temp;
+		}
+        System.out.println("After selection sort:");
+        for (int num : arr) 
+            System.out.print(num + " ");
+	}
+	
+	public static void main(String[] args) {
+		int arr[]= {3,4,5,2,1,7,6};
+		int n=arr.length;
+		selectionSort(arr,n);
+	}
+}
